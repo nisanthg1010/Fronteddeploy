228a1dc5ebf991e68681d9afbe7eda0db310544c
nisanthg1010 <nisanthg1010@gmail.com>
2025-01-06 11:27:51 +0530

    second commit

--- src/components/Apps.jsx
+++ src/components/Apps.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Createuser from './Createuser';
-import Updateuser from './updateuser';
+import Updateuser from './Updateuser';
 import User from  './user';
 
 const Apps = () => {
